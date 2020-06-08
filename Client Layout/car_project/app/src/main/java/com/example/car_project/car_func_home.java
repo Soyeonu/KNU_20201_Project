8d4b315ad4fb3f833c4a49beb5e9d416933cab0a
soyeonu <thdus1487@naver.com>
2020-06-08 21:57:00 +0900

    Changed Layout2
    xml files

--- Client Layout/car_project/app/src/main/java/com/example/car_project/car_func_home.java
+++ Client Layout/car_project/app/src/main/java/com/example/car_project/car_func_home.java
@@ -1,17 +1,27 @@
 package com.example.car_project;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.RequiresApi;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+
+import java.io.File;
 
 import me.relex.circleindicator.CircleIndicator;
 
@@ -20,27 +30,25 @@ import me.relex.circleindicator.CircleIndicator;
  * A simple {@link Fragment} subclass.
  */
 public class car_func_home extends Fragment implements ImageButton.OnClickListener{
-    View view;
-    private ViewPager mViewPager;
-    private ViewPagerFragmentAdapter mfragmentAdapter;
-    private ImageButton vButton, mButton;
+    private Activity activity;
 
     public car_func_home() {
         // Required empty public constructor
         // 에어컨과 비디오, 오디오를 포함한 클래스
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+
+//    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        view = inflater.inflate(R.layout.fragment_car_func_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_car_func_home, container, false);
 
-        mViewPager = view.findViewById(R.id.viewPager);
+        ViewPager mViewPager = view.findViewById(R.id.viewPager);
         mViewPager.setOffscreenPageLimit(3);
 
-        mfragmentAdapter = new ViewPagerFragmentAdapter(getChildFragmentManager());
+        ViewPagerFragmentAdapter mfragmentAdapter = new ViewPagerFragmentAdapter(getChildFragmentManager());
 
         mfragmentAdapter.addItem(new car_func_aircon());
         mfragmentAdapter.addItem(new Fragment1());
@@ -53,9 +61,9 @@ public class car_func_home extends Fragment implements ImageButton.OnClickListen
         CircleIndicator indicator = view.findViewById(R.id.indicator);
         indicator.setViewPager(mViewPager);
 
-        vButton = view.findViewById(R.id.videoBtn);
+        ImageButton vButton = view.findViewById(R.id.videoBtn);
         vButton.setOnClickListener(this);
-        mButton = view.findViewById(R.id.musicBtn);
+        ImageButton mButton = view.findViewById(R.id.musicBtn);
         mButton.setOnClickListener(this);
 
         return view;
@@ -63,6 +71,16 @@ public class car_func_home extends Fragment implements ImageButton.OnClickListen
 
     @Override
     public void onClick(View v) {
+
+//        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.READ_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED) {
+//            if (ActivityCompat.shouldShowRequestPermissionRationale(activity,Manifest.permission.READ_EXTERNAL_STORAGE)) {
+//
+//            } else {
+//                ActivityCompat.requestPermissions(activity,
+//                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},1);
+//            }
+//        }
+
         switch(v.getId()){
             case R.id.videoBtn:
                 // 비디오 버튼 클릭 시
@@ -82,9 +100,19 @@ public class car_func_home extends Fragment implements ImageButton.OnClickListen
                 intent.setType("audio/*");
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                 startActivity(Intent.createChooser(intent, "Open"));    // 파일을 열기만함.
+
                 break;
         }
     }
 
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        if (context instanceof Activity) {
+            activity = (Activity) context;
+        }
+
+    }
 }
